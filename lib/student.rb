426dbf96aa3cd2172573cd647eefd4a9308bc631
Michael Lynch <michael@michaelrlynch.com>
2019-10-31 16:09:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
       SELECT * FROM students WHERE grade = 10 LIMIT num
       SQL
 
-      DB[:conn].execute(sql, num).map do |row|
+      DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
       end
   end
